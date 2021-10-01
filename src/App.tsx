f3fe9e0f6b9832bcdc5e564818e9c28dc2ee3039
stmy <stmyzzz@outlook.com>
2021-10-01 23:05:13 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./styles/index.scss";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>demo</h1>
+        <h2>demo2</h2>
+        <h3>demo3</h3>
+        <code>codeererer</code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="blue"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
