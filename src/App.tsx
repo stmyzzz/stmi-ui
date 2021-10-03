e8a16cdf2fa83c0c4f4d6905c64f04adf4e72c81
stmy <stmyzzz@outlook.com>
2021-10-03 19:49:27 +0800

    添加Alert组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import "./styles/index.scss";
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Alert, { AlertTypes } from './components/Alert/alert'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>demo</h1>
-        <h2>demo2</h2>
-        <h3>demo3</h3>
-        <code>codeererer</code>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <hr />
         <Button >测试</Button>
         <Button disabled>disbaled</Button>
         <Button btnType={ButtonType.Danger}>警告按钮</Button>
@@ -19,8 +14,11 @@ function App() {
         <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>test2</Button>
         <Button btnType={ButtonType.Link} href="www.baidu.com" >link</Button>
         <Button btnType={ButtonType.Link} href="www.baidu.com" disabled >disabled link</Button>
+        <hr />
+        <Alert type="error" title="theme" closable={false}></Alert>
+        <Alert type="success" title="theme" closable={true}></Alert>
       </header>
-    </div>
+    </div >
   );
 }
 
